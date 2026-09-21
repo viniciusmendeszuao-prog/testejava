41f7f37a28e17a0291d5f8c433f7dd00d76466e7
viniciusmendeszuao-prog <viniciusmendeszuao@gmail.com>
2026-09-21 09:52:45 -0300

    calc

--- main.js
+++ main.js
@@ -27,3 +27,17 @@ function somar (x,y) {
     return c
 }
 */
+
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const v1 = Number(document.getElementById("num1").value)
+    const v2 = Number(document.getElementById("num2").value)
+
+    const soma = v1+v2
+
+    document.getElementById("resultado").textContent=soma
+
+})
