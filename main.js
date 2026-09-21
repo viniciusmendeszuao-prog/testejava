9da4c625128ed58a24db57c8cce260d19e63b5bf
viniciusmendeszuao-prog <viniciusmendeszuao@gmail.com>
2026-09-21 09:29:11 -0300

    calcSoma

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("teste")
+/* MODELO 1: EXEMPLO SIMPLES
+var a = 5
+var b = 7
+var c = a+b
+console.log(c)
+FIM DO MODELO 1*/
+
+/* MODELO 2: FUNÇÕES
+var a = 3
+var b = 4
+function somar (x,y){
+    var c = x+y
+    return c
+}
+
+console.log(somar(a,b))
+FIM DO MODELO 2*/
+
+/* MODELO 3: TIPOS DE VARIAVEIS
+var a = 7
+let b = 5
+const d = 0
+
+function somar (x,y) {
+
+    let c = x+y
+    return c
+}
+*/
